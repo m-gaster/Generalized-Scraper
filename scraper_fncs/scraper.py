6982091ee03f65601ec8521a19ed6b48f4ce3a0e
Mikhael Gaster <mikhaelgaster@gmail.com>
2022-04-04 20:04:32 -0700

    Should be finished

--- scraper_fncs/scraper.py
+++ scraper_fncs/scraper.py
@@ -1,6 +1,42 @@
 """
 THIS FILE DEFINES A LOW-LEVEL SCRAPING FUNCTION WHICH IS USED REPEATEDLY
+
+TEMPLATES CAN'T BE USED HERE, SINCE THIS DEFINES THE SPECIFIC SCRAPING ACTION WE WANT TO ACCOMPLISH
+
+HAS TO RETURN SOMETHING (usually pd.DataFrame)
 """
 
-def scraper():
-    ...
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import time
+
+from .update_checklist import update_checklist_table
+# "." is relative import :)
+
+def scrape_and_save(URL_to_scrape:str,
+                    scraped_data_save_path:str) -> int: 
+    """
+    This function grabs data from the URL.
+    MUST RETURN `True` if the scrape didn't work (i.e., if we want to log np.nan under "scraped_bool" field in 'checklist')
+    """
+    ... # scrape and save
+
+def scrape_and_update_checklist(checklist,
+                                scraped_bool_col:str,
+                                URL_col:str,
+                                URL_to_scrape:str,
+                                scraped_data_save_path:str,
+                                checklist_path:str,
+                                ):
+    """This is the main fnc which scrapes the data *AND* updates the checklist"""
+    log_as_nan = scrape_and_save(URL_to_scrape=URL_to_scrape,
+                                 scraped_data_save_path=scraped_data_save_path)
+    scrape_and_update_checklist(checklist=checklist,
+                                checklist_path=checklist_path,
+                                scraped_bool_col=scraped_bool_col,
+                                URL_col=URL_col,
+                                URL_of_newly_scraped=URL_to_scrape,
+                                log_as_nan=log_as_nan
+                                )
--- scraper_fncs/update_checklist.py
+++ scraper_fncs/update_checklist.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import numpy as np
 
-def update_checklist_table(checklist_path:str, 
+def update_checklist_table(checklist:pd.DataFrame,
+                           checklist_path:str, 
                            scraped_bool_col:str,
-                           ID_col:str,
-                           ID_of_newly_scraped:str,
+                           URL_col:str,
+                           URL_of_newly_scraped:str,
                            log_as_nan=False
                            ):
     
-    checklist = pd.read_csv(checklist_path)
+    # checklist = pd.read_csv(checklist_path)
 
     # confirm that bools are numerical and not True, False
     unique_bools = checklist[scraped_bool_col].unique().tolist()
     
-    if all(num_bool in unique_bools for num_bool in [0,1]):
-        pos_bool = 1
-    elif all(bool_ in unique_bools for bool_ in [True, False]):
-        pos_bool = True
-    
     if log_as_nan:
         pos_bool = np.nan
+    else:
+        if not any(num_bool in unique_bools for num_bool in [0,1]): # if 0 and 1 aren't in 
+            assert any(bool_ in unique_bools for bool_ in [True, False])
+            pos_bool = True
+        elif not any(bool_ in unique_bools for bool_ in [True, False]):
+            assert any(num_bool in unique_bools for num_bool in [0,1])
+            pos_bool = 1
+
 
     # update value
-    checklist.set_index(ID_col, 
+    checklist.set_index(URL_col, 
                         inplace=True)
-    checklist.loc[ID_of_newly_scraped, scraped_bool_col] = 1
+    checklist.loc[URL_of_newly_scraped, scraped_bool_col] = pos_bool
 
     # save/export
     checklist.to_csv(checklist_path,
